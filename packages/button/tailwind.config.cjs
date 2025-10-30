9875ff1047d7862570e27dce54fa6343e328a5f9
Humberto Canales <humberto.canales@siticsoftware.com>
2025-10-30 03:28:09 -0600

    feat(core): se agrega prueba de compilado de estilos

---
+++ packages/button/tailwind.config.cjs
@@ -0,0 +1,14 @@
+/**** Tailwind config for the Button package ****/
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  corePlugins: {
+    preflight: false, // avoid resetting consumer app styles
+  },
+  plugins: [],
+};
